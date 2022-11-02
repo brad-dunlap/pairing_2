65d472eab0694024c66400d9bbead1ca5c50ef52
Brad Dunlap <braddunlapdpt@gmail.com>
2022-11-02 18:34:41 -0400

    Finish pairing 2

--- friends.rb
+++ friends.rb
@@ -1,6 +1,13 @@
 best_friend = "Ben"
+good_friend = "Sally"
+
 if best_friend == "Bob"
     puts "Hi, Bob"
+elsif good_friend == "Steve"
+    puts "What's up Steve?"
+
 else 
     puts "You're not Bob! You must be #{best_friend}."
 end
+
+
